75f7245fb137d275931b4e2c073b1a7c640acd1c
nbrowning1 <n.lalilulelo@gmail.com>
2017-10-07 23:15:28 +0100

    Test commit to see if username change worked

--- tool/views.py
+++ tool/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Student#, Mark
+from .models import Student#, Mark 
 
 from graphos.sources.simple import SimpleDataSource
 from graphos.renderers.gchart import LineChart
