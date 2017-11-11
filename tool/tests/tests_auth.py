e5105cb6e7429602f529c86fe020e5a11f37f3db
nbrowning1 <n.browning13@gmail.com>
2017-11-11 20:13:59 +0000

    Added tests for upload, tiny tweaks to auth views for login links

--- tool/tests/tests_auth.py
+++ tool/tests/tests_auth.py
@@ -7,16 +7,43 @@ from django.core import mail
 from django.contrib.auth.models import User
 
 class AuthTests(TestCase):
+  
+  def test_login_valid_credentials(self):
+    user = User.objects.create_superuser('john', 'test@mail.com', 'johnpassword')
+    test_login(self, 'john', 'johnpassword', True)
+    
+  def test_login_invalid_credentials(self):
+    user = User.objects.create_superuser('john', 'test@mail.com', 'johnpassword')
+    test_login(self, 'john', 'wrongpassword', False)
+    
+  def test_login_nonexistent_user(self):
+    test_login(self, 'nouser', 'nopassword', False)
     
   def test_password_reset_recognised_email(self):
     user = User.objects.create_user('john', 'test@mail.com', 'johnpassword')
-    invoke_password_reset(self, 'test@mail.com', True)
+    test_password_reset(self, 'test@mail.com', True)
     
   def test_password_reset_unrecognised_email(self):
     user = User.objects.create_user('john', 'test@mail.com', 'johnpassword')
-    invoke_password_reset(self, 'different@mail.com', False)
+    test_password_reset(self, 'different@mail.com', False)
     
-def invoke_password_reset(self, email_address, email_expected):
+def test_login(self, username, password, success_expected):
+  response = self.client.post(reverse('tool:login'), {'username':username, 'password':password})
+  if success_expected:
+    self.assertEqual(response.status_code, 302)
+  else:
+    self.assertEqual(response.status_code, 200)
+
+  response = self.client.get(reverse('tool:index'))
+  if success_expected:
+    # should be able to see index now
+    self.assertEqual(response.status_code, 200)
+  else:
+    # or be redirected to login
+    self.assertEqual(response.status_code, 302)
+  
+    
+def test_password_reset(self, email_address, email_expected):
   # post the response with our email address
   response = self.client.post(reverse('tool:password_reset'),{'email':email_address})
   self.assertEqual(response.status_code, 302)
---
+++ tool/tests/tests_upload.py
@@ -0,0 +1,84 @@
+from django.test import TestCase
+
+from django.urls import reverse
+
+from django.core import mail
+
+from django.contrib.auth.models import User
+
+from ..models import Student, Staff, Module, Lecture, StudentAttendance
+
+import os
+
+class UploadTests(TestCase):
+  
+  def test_upload_valid_data(self):
+    # add extra assertions to check that links weren't created before but are after upload, also for upload confirmation page
+    create_student('B00123456')
+    create_student('B00987654')
+    create_staff('A00123456')
+    create_staff('A00987654')
+    create_module('COM101')
+    create_module('COM999')
+    test_upload(self, 'upload_test_valid.csv', None)
+  
+  def test_upload_unrecognised_data(self):
+    # uploading valid data but with unrecognised module, staff, student
+    test_upload(self, 'upload_test_valid.csv', 'Error with inputs: [[Unrecognised module: COM101, Unrecognised lecturer: A00123456, Unrecognised student: B00123456]] at line 1')
+  
+  def test_upload_incorrect_file_extension(self):
+    test_upload(self, 'upload_test_wrong_ext.txt', 'Invalid file type. Only csv files are accepted.')
+    
+  def test_upload_no_file(self):
+    test_upload(self, None, 'No file uploaded. Please upload a .csv file.')
+    
+def test_upload(self, file_path, expected_error_msg):
+  authenticate_admin(self)
+    
+  response = self.client.get(reverse('tool:index'))
+  self.assertEqual(response.status_code, 200)
+  if expected_error_msg:
+    self.assertNotContains(response, expected_error_msg)
+
+  this_dir = os.path.dirname(os.path.abspath(__file__));
+  # if no path specified, post without any upload data
+  if file_path:
+    res_path = os.path.join(this_dir, 'resources', file_path)
+    with open(res_path) as fp:
+      response = self.client.post(reverse('tool:upload'), {'upload-data': fp}, follow=True)
+  else:
+    res_path = ''
+    response = self.client.post(reverse('tool:upload'), follow=True)
+  
+  self.assertEqual(response.status_code, 200)
+  
+  # if no error message expected, check for successfully making it to upload confirmation page
+  if expected_error_msg:
+    self.assertEqual(response.request['PATH_INFO'], '/tool/')
+    self.assertContains(response, expected_error_msg)
+  else:
+    self.assertEqual(response.request['PATH_INFO'], '/tool/upload/')
+    
+  return response
+    
+def authenticate_admin(self):
+  user = User.objects.create_superuser(username='test', password='12345', email='test@mail.com')
+  self.client.login(username='test', password='12345')
+  return user
+
+def create_student(username):
+  user = User.objects.create_user(username=username, password='12345')
+  student = Student(user=user)
+  student.save()
+  return student
+  
+def create_staff(username):
+  user = User.objects.create_user(username=username, password='12345')
+  staff = Staff(user=user)
+  staff.save()
+  return staff
+
+def create_module(module_code):
+  module = Module(module_code=module_code)
+  module.save()
+  return module
